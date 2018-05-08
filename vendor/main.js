fe3dd39e5ae0677fec93f3d019db39807cd38184
Joe Carre <joe.carre@rfa.co.uk>
2018-05-08 13:59:52 +0100

    bulletin delete button hover
    -removed the increased width when the user hovers over delete button
    -button now only appears when user hovers over button as a whole

--- vendor/main.js
+++ vendor/main.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+    $(".bulletinContainer").mouseover(function() {
+    	$(this).find(".removeBulletinBtn").css("display", "block");
+    });
+
+    $(".bulletinContainer").mouseleave(function() {
+    	$(this).find(".removeBulletinBtn").css("display", "none");
+    });
 });
