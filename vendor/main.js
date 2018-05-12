1ea8f552fee0cf3c5ec63fedd61b272604270acc
Joe <carre.joe3@gmail.com>
2018-05-12 19:01:03 +0100

    Started the load bullet function

--- vendor/main.js
+++ vendor/main.js
@@ -3,11 +3,32 @@ $( document ).ready(function() {
         setBulletin();
     });
 
-    $("#submitBtn").click(function () {
+    $("#submitBtn").click(function() {
         getBulletins();
     });
 
-    $(".addBulletinBtn").click(function () {
-        $("#bulletinList").append("<li class='bulletinListItem'>New</li><hr class='listItemBottomBorder'>");
+    $(".addBulletinBtn").click(function() {
+        let x = "<li class='bulletinListItem' data-bulletinId=''>New</li><hr class='listItemBottomBorder'>";
+
+        //give bulletin list item an id
+        x = x.substring(0, 46) + generateUUID() + x.substring(46, x.length);
+        $("#bulletinList").append(x);
+    });
+
+    $("#bulletinList").on("click", ".bulletinListItem", function() {
+        let bulletinId = $(this).attr("data-bulletinId");
+        //call getBulletin() and set bulletin fields to returned values
     });
 });
+
+function generateUUID() {
+    var d = new Date().getTime();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+        d += performance.now();
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
