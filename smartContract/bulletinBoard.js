a34c4ba6e91cf7ceb00b59afe7577553a1218092
Joe <carre.joe3@gmail.com>
2018-05-27 19:35:26 +0100

    Send smart contract/handler
    -added sendTo function to smart contract and smart contract handler

--- smartContract/bulletinBoard.js
+++ smartContract/bulletinBoard.js
@@ -6,15 +6,23 @@ var BulletinBoard = function () {
 BulletinBoard.prototype = {
     init: function () {
     },
-    setBulletins: function (idList, titles, contents, createdDates, owner) {
+    setBulletins: function (idList, titles, contents, createdDates) {
         var author = Blockchain.transaction.from;
         titles = titles.trim();
         contents = contents.trim();
         LocalContractStorage.set(author, {ids: idList, titles: titles, contents: contents, createdDates: createdDates});
     },
-    getBulletins: function (owner) {
+    sendBulletins: function (idList, titles, contents, createdDates, owner) {
+        titles = titles.trim();
+        contents = contents.trim();
+        LocalContractStorage.set(owner, {ids: idList, titles: titles, contents: contents, createdDates: createdDates});
+    },
+    getBulletins: function () {
         var author = Blockchain.transaction.from;
         return LocalContractStorage.get(author);
+    },
+    getSpecificBulletins: function (owner) {
+        return LocalContractStorage.get(owner);
     },
     delBulletins: function () {
         var author = Blockchain.transaction.from;
